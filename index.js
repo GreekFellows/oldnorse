5122a1e503051013084fc8585cc25886383d9137
GreekFellows <unknownuser826@gmail.com>
2015-03-28 18:49:50 +0800

    Framework written.
    Framework written; time for contents.

---
+++ index.js
@@ -0,0 +1,20 @@
+$("#tabhome a").click(
+	function (event) {
+		event.preventDefault();
+		$(this).tab("show");
+	}
+);
+
+$("#tabprof a").click(
+	function (event) {
+		event.preventDefault();
+		$(this).tab("show");
+	}
+);
+
+$("#tabmsgs a").click(
+	function (event) {
+		event.preventDefault();
+		$(this).tab("show");
+	}
+);
